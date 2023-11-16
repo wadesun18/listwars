7bec02f392d1ea71e00ce2d2c7c6866f5e13d576
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-11-16 12:41:23 -0800

    added and tested Montserrat fonts

--- src/screen/LaunchScreen.tsx
+++ src/screen/LaunchScreen.tsx
@@ -1,34 +1,79 @@
 import React from 'react';
-import {Image} from 'react-native';
-import Onboarding from 'react-native-onboarding-swiper';
+import {StyleSheet, Text, View} from 'react-native';
+// import {Image} from 'react-native';
+// import Onboarding from 'react-native-onboarding-swiper';
 
 function LaunchScreen() {
   return (
-    <Onboarding
-      onDone={() => console.log('done')}
-      bottomBarHighlight={false}
-      pages={[
-        {
-          backgroundColor: '#fff',
-          image: <Image source={require('../images/LW_Logo1024x1024.jpg')} />,
-          title: 'Onboarding',
-          subtitle: 'Done with React Native Onboarding Swiper',
-        },
-        {
-          backgroundColor: '#fe6e58',
-          image: <Image source={require('../images/LW_Logo1024x1024.jpg')} />,
-          title: 'The Title',
-          subtitle: 'This is the subtitle that sumplements the title.',
-        },
-        {
-          backgroundColor: '#999',
-          image: <Image source={require('../images/LW_Logo1024x1024.jpg')} />,
-          title: 'Triangle',
-          subtitle: "Beautiful, isn't it?",
-        },
-      ]}
-    />
+    <View style={styles.container}>
+      <Text style={styles.montserratBold}>This text uses Montserrat bold</Text>
+      <Text style={styles.montserratRegular}>
+        This text uses Montserrat Regular
+      </Text>
+      <Text style={styles.montserratBoldItalic}>
+        This is Montserrat Bold Italic
+      </Text>
+      <Text style={styles.montserratSemiBold}>
+        This is Montserrat Semi Bold
+      </Text>
+      <Text style={styles.montserratSemiBoldItalic}>
+        This is Montserrat Semi Bold
+      </Text>
+    </View>
+    // <Onboarding
+    //   onDone={() => console.log('done')}
+    //   bottomBarHighlight={false}
+    //   pages={[
+    //     {
+    //       backgroundColor: '#fff',
+    //       image: <Image source={require('../images/LW_Logo1024x1024.jpg')} />,
+    //       title: 'Onboarding',
+    //       subtitle: 'Done with React Native Onboarding Swiper',
+    //     },
+    //     {
+    //       backgroundColor: '#fe6e58',
+    //       image: <Image source={require('../images/LW_Logo1024x1024.jpg')} />,
+    //       title: 'The Title',
+    //       subtitle: 'This is the subtitle that sumplements the title.',
+    //     },
+    //     {
+    //       backgroundColor: '#999',
+    //       image: <Image source={require('../images/LW_Logo1024x1024.jpg')} />,
+    //       title: 'Triangle',
+    //       subtitle: "Beautiful, isn't it?",
+    //     },
+    //   ]}
+    // />
   );
 }
 
 export default LaunchScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'lavender',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  montserratRegular: {
+    fontFamily: 'Montserrat-Regular',
+    fontSize: 20,
+  },
+  montserratBold: {
+    fontFamily: 'Montserrat-Bold',
+    fontSize: 20,
+  },
+  montserratBoldItalic: {
+    fontFamily: 'Montserrat-BoldItalic',
+    fontSize: 20,
+  },
+  montserratSemiBold: {
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 20,
+  },
+  montserratSemiBoldItalic: {
+    fontFamily: 'Montserrat-SemiBoldItalic',
+    fontSize: 20,
+  },
+});
