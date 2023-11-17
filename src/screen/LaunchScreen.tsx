88776c69f8b33ce4aedf57e428a81ea2d5a39c1f
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-11-16 21:23:11 -0800

    minor changes

--- src/screen/LaunchScreen.tsx
+++ src/screen/LaunchScreen.tsx
@@ -10,19 +10,20 @@ function LaunchScreen() {
       pages={[
         {
           backgroundColor: '#fff',
-          image: <Image source={require('../images/LW_Logo1024x1024.jpg')} />,
-          title: 'Onboarding',
-          subtitle: 'Done with React Native Onboarding Swiper',
+          image: <Image source={require('../images/LW_Logo_example.jpg')} />,
+          title: 'Create a List',
+          subtitle:
+            'Create a list of your choice: todo list, shopping list or house chores list',
         },
         {
           backgroundColor: '#fe6e58',
-          image: <Image source={require('../images/LW_Logo1024x1024.jpg')} />,
+          image: <Image source={require('../images/LW_Logo_example.jpg')} />,
           title: 'The Title',
           subtitle: 'This is the subtitle that sumplements the title.',
         },
         {
           backgroundColor: '#999',
-          image: <Image source={require('../images/LW_Logo1024x1024.jpg')} />,
+          image: <Image source={require('../images/LW_Logo_example.jpg')} />,
           title: 'Triangle',
           subtitle: "Beautiful, isn't it?",
         },
