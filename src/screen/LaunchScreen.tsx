6fd98a2c19c78d1cabc59542788a0b5063af5085
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-11-20 17:11:44 -0800

    removed unneeded image and import

--- src/screen/LaunchScreen.tsx
+++ src/screen/LaunchScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, StyleSheet} from 'react-native';
+import {Image} from 'react-native';
 import Onboarding from 'react-native-onboarding-swiper';
 import styled from 'styled-components/native';
 
