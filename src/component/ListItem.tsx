e549c1d4cb9e0681cb0d9f72e4fd20604c7e1b7c
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-11-22 13:29:29 -0800

    implemented flatlist to render list titles

--- src/component/ListItem.tsx
+++ src/component/ListItem.tsx
@@ -2,6 +2,7 @@ import {Text} from 'react-native';
 
 import {List} from '../../types/data';
 
-export default function ListItem(item: List) {
+export default function ListItem({item}: {item: List}) {
+  console.log('List', item);
   return <Text>{item.listName}</Text>;
 }
--- src/screen/HomeScreen/HomeScreen.tsx
+++ src/screen/HomeScreen/HomeScreen.tsx
@@ -1,18 +1,32 @@
-import {Text, View} from 'react-native';
+import {FlatList, View} from 'react-native';
 
 import ListItem from '../../component/ListItem';
-import {
-  ExampleListOne,
-  ExampleListThree,
-  ExampleListTwo,
-} from '../../data/MockData';
+import {Data} from '../../data/MockData';
 
 export default function HomeScreen() {
   return (
     <View style={{marginTop: 50}}>
-      <ListItem {...ExampleListOne} />
-      <ListItem {...ExampleListTwo} />
-      <ListItem {...ExampleListThree} />
+      <FlatList
+        data={Data}
+        renderItem={({item}) => <ListItem item={item} />}
+        keyExtractor={item => item.listName}
+      />
     </View>
   );
 }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     marginTop: StatusBar.currentHeight || 0,
+//   },
+//   item: {
+//     backgroundColor: '#f9c2ff',
+//     padding: 20,
+//     marginVertical: 8,
+//     marginHorizontal: 16,
+//   },
+//   title: {
+//     fontSize: 32,
+//   },
+// });
