833730fb3dcad883c543edb4dbba87278b0ccabc
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-11-25 19:07:36 -0800

    Tweak UI

--- src/component/ListItem.tsx
+++ src/component/ListItem.tsx
@@ -1,27 +1,84 @@
+import {faCheck} from '@fortawesome/free-solid-svg-icons/faCheck';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import styled from 'styled-components/native';
 
 import {Task} from '../../types/data';
+import {COMPLETE_COLOR, LIST_COLOR} from '../constants';
 
-const RowText = styled.Text`
+const ListTitle = styled.Text<{status: string}>`
+  font-family: Montserrat-Regular;
+  font-size: 24px;
+  font-weight: 800;
+  margin-bottom: 2px;
+  text-decoration-line: ${props =>
+    props.status === 'complete' && 'line-through'};
+  color: ${props =>
+    props.status === 'complete' ? COMPLETE_COLOR : LIST_COLOR};
+`;
+
+const ListDetails = styled.Text<{status: string}>`
   color: blue;
   font-family: Montserrat-Regular;
   font-size: 14px;
+  font-weight: 400;
   margin-bottom: 2px;
+  color: #fff;
+  text-decoration-line: ${props =>
+    props.status === 'complete' && 'line-through'};
 `;
 
 const RowView = styled.View`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
+  height: 80px;
 `;
 
-const FIELDS = ['item', 'details', 'whodunnit', 'status'];
-export default function ListItem({item}: {item: Task}) {
+const TaskContainer = styled.View`
+  flex: 1;
+`;
+
+const DoneButton = styled.TouchableOpacity`
+  background-color: #000;
+  height: 50px;
+  width: 50px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 6px;
+`;
+
+const DoneByText = styled.Text`
+  font-family: Montserrat-Regular;
+  font-size: 14px;
+  font-weight: 400;
+  color: ${LIST_COLOR};
+`;
+
+export default function ListItem({
+  item: {title, details, status, whodunnit},
+}: {
+  item: Task;
+}) {
   return (
     <RowView>
-      {FIELDS.map((field: string) => {
-        return <RowText>{item[field as keyof Task]}</RowText>;
-      })}
+      <TaskContainer>
+        <ListTitle status={status}>{title}</ListTitle>
+        {status === 'incomplete' && (
+          <ListDetails status={status}>{details}</ListDetails>
+        )}
+      </TaskContainer>
+      {status === 'incomplete' ? (
+        <DoneButton>
+          <FontAwesomeIcon
+            icon={faCheck}
+            style={{color: '#fca903'}}
+            size={32}
+          />
+        </DoneButton>
+      ) : (
+        <DoneByText>{whodunnit}</DoneByText>
+      )}
     </RowView>
   );
 }
---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const LIST_COLOR = '#fca903';
+export const COMPLETE_COLOR = '#969592';
